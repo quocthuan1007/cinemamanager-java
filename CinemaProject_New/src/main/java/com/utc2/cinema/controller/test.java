9ef1e4569c94c188795f4e6f71990312e7412568
BTTBao <lebao.071205@gmail.com>
2025-04-14 10:39:11 +0700

    1

--- CinemaProject_New/src/main/java/com/utc2/cinema/controller/test.java
+++ CinemaProject_New/src/main/java/com/utc2/cinema/controller/test.java
@@ -12,13 +12,19 @@ public class test {
     // Phương thức xử lý khi nút được nhấn
     @FXML
     private void handleVideoButtonClick() {
-        // URL nhúng của video YouTube
-        String videoURL = "https://www.youtube.com/embed/abc123";  // Thay "abc123" bằng ID video thực tế
+        String videoID = "HXWRTGbhb4U";  // ID video YouTube
+        String embedHTML = """
+        <html>
+            <body style='margin:0px;padding:0px;'>
+                <iframe width='100%%' height='100%%' 
+                        src='https://www.youtube.com/embed/%s?autoplay=1'
+                        frameborder='0' allow='autoplay; encrypted-media' allowfullscreen>
+                </iframe>
+            </body>
+        </html>
+        """.formatted(videoID);
 
-        // Lấy WebEngine từ WebView
         WebEngine webEngine = webView.getEngine();
-
-        // Tải URL video vào WebView
-        webEngine.load(videoURL);
+        webEngine.loadContent(embedHTML);
     }
 }
