00b29381b8fdaec421a75c1fc02932c46d2c32fb
BTTBao <lebao.071205@gmail.com>
2025-07-22 15:42:16 +0700

    fix loi nhe

--- CinemaProject_New/src/main/java/com/utc2/cinema/model/entity/EmailOTP.java
+++ CinemaProject_New/src/main/java/com/utc2/cinema/model/entity/EmailOTP.java
@@ -7,13 +7,14 @@ import java.util.Properties;
 public class EmailOTP {
     public static void sendEmail(String to, String title, String content) throws Exception {
         String from = "minecraftchannel510@gmail.com";
-        String password = "mlzf kxqe glfw wfcp";
+        String password = "mlzf kxqe glfw wfcp"; // App password từ Google
 
         Properties props = new Properties();
         props.put("mail.smtp.auth", "true");
         props.put("mail.smtp.starttls.enable", "true");
         props.put("mail.smtp.host", "smtp.gmail.com");
         props.put("mail.smtp.port", "587");
+        props.put("mail.smtp.ssl.trust", "smtp.gmail.com"); // ✅ Bypass xác minh SSL
 
         Session session = Session.getInstance(props, new Authenticator() {
             protected PasswordAuthentication getPasswordAuthentication() {
@@ -28,12 +29,12 @@ public class EmailOTP {
 
         String htmlContent = "<!DOCTYPE html>"
                 + "<html><body style='font-family:Arial,sans-serif;'>"
-                + "<div style='max-width:500px;" +
-                "margin:auto;" +
-                "padding:20px;" +
-                "border:1px solid #ccc;" +
-                "border-radius:10px;" +
-                "background-color:#61C17E;'>"
+                + "<div style='max-width:500px;"
+                + "margin:auto;"
+                + "padding:20px;"
+                + "border:1px solid #ccc;"
+                + "border-radius:10px;"
+                + "background-color:#61C17E;'>"
                 + "<h2 style='color:#333;'>Thông báo từ hệ thống</h2>"
                 + "<p style='font-size:20px;color:#FFFFFF;'>" + content + "</p>"
                 + "<br><p style='font-size:14px;color:#999;'>Nếu bạn không yêu cầu, hãy bỏ qua email này.</p>"
@@ -42,8 +43,6 @@ public class EmailOTP {
         msg.setContent(htmlContent, "text/html; charset=UTF-8");
 
         Transport.send(msg);
-        System.out.println("Email đã gửi tới " + to);
+        System.out.println("✅ Email đã gửi tới " + to);
     }
-
 }
-
